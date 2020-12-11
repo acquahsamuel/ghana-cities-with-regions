0a253f19bbacc840d0eadd895c290170e406296c
acquahsamuel <acquahsamuel1996@gmail.com>
2020-12-11 11:20:46 +0000

    ghana cities api

--- utils/appError.js
+++ utils/appError.js
@@ -5,11 +5,10 @@ class AppError extends Error{
         this.statusCode = statusCode;
         this.status =  `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
-        
+
         Error.captureStackTrace(this, this.constructor);
     }
 }
-
 
 module.exports = AppError;
 
