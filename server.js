7e55c91f449a3b7c59892fe65a5fba22f5564508
acquahsamuel <acquahsamuel1996@gmail.com>
2021-04-07 06:31:52 +0000

    server.js

--- server.js
+++ server.js
@@ -1,106 +1,103 @@
-const path = require('path')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const colors = require('colors')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const express = require('express')
-const hpp = require('hpp')
-const cors = require('cors')
-const fileupload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const mongoSanitize = require('express-mongo-sanitize')
-const rateLimit = require('express-rate-limit')
-const connectDB = require('./config/db')
+const path = require('path');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const express = require('express');
+const hpp = require('hpp');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const connectDB = require('./config/db');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 // Load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({path: './config/config.env'});
 
 // Connect to database
-connectDB()
+connectDB();
 
 // Route files
-const cities = require('./routes/citiesRoutes')
-const regions = require('./routes/regionsRoutes')
+const cities = require('./routes/citiesRoutes');
+const regions = require('./routes/regionsRoutes');
 
-const app = express()
+const app = express();
 
 // Body parser
-app.use(express.json())
+app.use(express.json());
 
 // Cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
 // File uploading
-app.use(fileupload())
+app.use(fileupload());
 
 // Sanitize data
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Set security headers
-app.use(helmet())
+app.use(helmet());
 
 // Prevent XSS attacks
-app.use(xss())
+app.use(xss());
 
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 mins
-  max: 100
-})
-app.use(limiter)
+  max: 100,
+});
+app.use(limiter);
 
 // Prevent http param pollution
-app.use(hpp())
+app.use(hpp());
 
 // Enable CORS
-app.use(cors())
+app.use(cors());
 
 // Set static folder
-app.set('view engine' , 'ejs');
-app.set('views' , path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
-app.use('/api/v1/cities', cities)
-app.use('/api/v1/regions', regions)
+app.use('/api/v1/cities', cities);
+app.use('/api/v1/regions', regions);
 
-
-app.get('/' ,(req, res)=>{
+app.get('/', (req, res) => {
   res.render('index');
-})
+});
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Ooop's can't find ${req.originalUrl}, ${req.method} on this server` , 404));
-})
-
+  next(
+    new AppError(
+      `Ooop's can't find ${req.originalUrl}, ${req.method} on this server`,
+      404
+    )
+  );
+});
 
 app.use(globalErrorHandler);
 
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-)
+);
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red)
-})
-
-
-
-
-
+  console.log(`Error: ${err.message}`.red);
+});
 
 
